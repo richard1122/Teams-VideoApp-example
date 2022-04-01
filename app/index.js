21e8ccca3e9f91d108cf548b84eef593c14873af
Richard He <richard9372@gmail.com>
2022-04-01 17:36:35 +0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -15,17 +15,6 @@ let uiSelectedEffect = {};
 let errorOccurs = false;
 //Sample video effect
 function videoFrameHandler(videoFrame, notifyVideoProcessed, notifyError) {
-  if (uiSelectedEffect.proportion != 0) {
-    const maxLen =
-      (videoFrame.height * videoFrame.width) /
-        Math.max(1, appliedEffect.proportion) - 4;
-
-    for (let i = 1; i < maxLen; i += 4) {
-      //smaple effect just change the value to 100, which effect some pixel value of video frame
-      videoFrame.data[i + 1] = appliedEffect.pixelValue;
-    }
-  }
-
   //send notification the effect processing is finshed.
   notifyVideoProcessed();
 
